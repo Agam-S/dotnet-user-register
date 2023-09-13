8c75784b4e3594a6e7cea6572418377a19c4f766
Agam-S <singh.agam0009@gmail.com>
2023-09-14 09:05:17 +1000

    get all users - done

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 namespace dotnet_user_register.Controllers;
-using dotnet_user_register.models;
 
+using System.Data;
+using dotnet_user_register.models;
+using Microsoft.Data.Sqlite;
 
 [ApiController]
 [Route("[controller]")]
@@ -16,10 +18,44 @@ public class UserController : ControllerBase
     }
 
     [HttpGet(Name = "GetUsers")]
-    public String GetUser()
+    public IEnumerable<User> GetUsers()
     {
-        return "Hello";
-       
+    List<User> userList = new List<User>();
+    User listedUser = new User();
+
+    var connection = new SqliteConnection("Data Source=User.db");
+    
+    connection.Open();
+    
+    var command = connection.CreateCommand();
+
+    command.CommandText =
+    @"
+        SELECT *
+        FROM user
+    ";
+
+    using (var reader = command.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+            var name = reader.GetString(0);
+            var userName = reader.GetString(1);
+            var email = reader.GetString(2);
+            var password = reader.GetString(3);
+
+            Console.WriteLine($"{name} - {userName}");
+            
+            listedUser.Name = name;
+            listedUser.UserName = userName;
+            listedUser.Email = email;
+            listedUser.Password = password;
+
+            userList.Add(listedUser);
+
+        }
+    }
+    return userList;
     }
     [HttpPost(Name = "CreateUser")]
     public String CreateUser() {
